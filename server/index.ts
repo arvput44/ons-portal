8276eb4280c98767e59017b148be096222a83a68
Arvind <arvind@BGINDLM13LP0043.local>
2025-09-09 22:08:15 +0530

    making dynami will start after this

--- server/index.ts
+++ server/index.ts
@@ -60,11 +60,11 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 6000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '6000', 10);
+  const port = parseInt('3000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    // host: "0.0.0.0",
+    // reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
   });
